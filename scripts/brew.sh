34729526d6620d2ac7b60da738ca384db149b53e
Craig Valentine <craigvalentine91@googlemail.com>
2020-03-09 21:08:33 +0000

    Add script to install applications from Homebrew

---
+++ scripts/brew.sh
@@ -0,0 +1,24 @@
+# Download latest updates to Homebrew
+brew update
+
+# Install CLI tools
+brew install \
+    git \
+    go \
+    hugo \
+    python \
+    rbenv \
+    tree \
+    yarn
+
+# Install macOS applications
+brew cask install \
+    1password \
+    docker \
+    firefox \
+    iterm2 \
+    google-chrome \
+    nordvpn \
+    spotify \
+    visual-studio-code \
+    vlc
