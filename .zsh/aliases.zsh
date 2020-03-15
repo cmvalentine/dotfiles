529a10f47db88089657d06f72443e02467306b1e
Craig Valentine <craigvalentine91@googlemail.com>
2020-03-15 12:07:21 +0000

    Changed alias to better reflect what it does

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 alias brewup='brew update && brew upgrade && brew cleanup'
-alias nvmup='nvm install node --reinstall-packages-from=node'
+alias nodeup='nvm install node --reinstall-packages-from=node'
